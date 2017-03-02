b9cfea8f2c71f55f21348b1d87fc7ade6a50e5f3
Bilal <bilalcorbacioglu@gmail.com>
2017-03-02 20:25:56 +0300

    code review

--- Sources/secret.cpp
+++ Sources/secret.cpp
@@ -1,4 +1,3 @@
-//Programda, her koşulda 251 tabanında çalıştığımıza lütfen dikkat edin !!!
 #include "secret.h"
 #include <iostream>
 #include <string>
@@ -13,9 +12,8 @@
 #include "bitmap_image.hpp"
 using namespace std;
 int yedek;
-int subline; //alicagim yedek class'a ait bir degisken olmamali
+int subline;
 int width,height;
-//Satır sayısını alıyoruz ve next toplam satır sayımızı veriyor.
 
 void secret::createtxt(){
   FILE *streamIn;
@@ -27,13 +25,10 @@ void secret::createtxt(){
   if (streamIn == (FILE *)0) {
     printf("File opening error ocurred. Exiting program.\n");
   }
-  
   unsigned char info[54];
   fread(info, sizeof(unsigned char), 54, streamIn);
-  
   int width = *(int*)&info[18];
   int height = *(int*)&info[22];
-  
   int image[width*height][3];
   int i = 0;
   FILE *fw;
@@ -44,9 +39,7 @@ void secret::createtxt(){
     image[i][0] = getc(streamIn);
     printf("pixel %d : [%d,%d,%d]\n",i+1,image[i][0],image[i][1],image[i][2]);
     fprintf(fw,"%d\n%d\n%d\n",image[i][0],image[i][1],image[i][2]);
-    
   }
-  
   fclose(streamIn);
 }
 
@@ -62,7 +55,6 @@ void secret::satirsayisi(){
   subline = next;
 }
 
-
 void secret::sayigir(){
   createtxt();
   ifstream file("pikseller.txt");
@@ -75,11 +67,9 @@ void secret::sayigir(){
     paydasec();
     fonksyonbul();
   }}
-  
-  
+
   //Kullanıcıdan şemayı aldım SEMA(SEMAX,SEMAY)
   void secret::semagir(){
-    
     cout << endl << "Semayı girin (kullanicilar,dagitilacaklar)" << endl << " Kullanicilar:\t";
     cin >> semax;
     cout << endl << "Dagitacilaklar:\t";
@@ -91,8 +81,7 @@ void secret::sayigir(){
     cin >> height;
     cout<<endl;
   }
-  
-  
+
   //Sır paylaşımı alg. gereği fonksyon atamamızı yapıyoruz. Her bir piksele ayrı ayrı fonksyon ataması oluyor.
   void secret::fonksyonata(){
     i = 0;
@@ -101,8 +90,7 @@ void secret::sayigir(){
       random[i] = rand() % 4; // 251 e gore mod aldigimizda 12 8 icin mesela goruntu bozuluyor.
     }
   }
-  
-  
+
   //Kaç kişi varsa o kadar payda(pay) hesaplanıyor.Ardından payda yani hangi kişilere dağıtılacağı seçilecek.
   void secret::paydahesapla(){
     i = 0,toplam = 0;
@@ -116,12 +104,10 @@ void secret::sayigir(){
       partition[i] = (toplam+value) % 251;	//251 e göre mod aldım çünkü çakışmaları önlemem gerekiyor.
     }
   }
-  
-  
+
   //Aşağıda hangi kişilere hangi payda(pay) degerlerinin dağıtılacağı seçiliyor. Rastgele secim yaptırıyorum.
   void secret::paydasec(){
     selectedpartition = new int [semay];
-    
     for(i =0;i<semay;i++){
       rastgele = rand() %semax+1;
       if(i>0){
@@ -133,32 +119,27 @@ void secret::sayigir(){
       else selectedpartition[i] = rastgele;
     }
   }
-  
-  
+
   //Lagrange İnterpolasyondan çıkan sonuç double ise integer'a çeviriyorum.251 tabanında çalıştığıma dikkat edin !
   void secret::oylemi(double interpolasyon){
     while(1) {
       if((interpolasyon-(int)interpolasyon) != 0) {
         pay = pay+251; interpolasyon = (pay/payda);
       }
-      
       else {
         interpolasyonint = (int)interpolasyon;
         break;
       }
     }
   }
-  
+
   //Kullanıcıların alacağı yeni piksel değerleri aşağıda hesaplanıp .txt yazılacak.
   void secret::fonksyonbul(){
-    
     //Interpolasyon islemine baslaniyor
     //Asagidaki ic ice bulunan for dongusunu daha rahat kavramak icin Lagrange Interpolasyonuna goz atin.
-    
     for(i = 0; i < semay; i++){
       ofstream dosya;
       payda = 1, pay = 1; //Interpolasyonda matematiksel olarak pay ve payda
-      
       for(j = 0; j < semay; j++){
         if( selectedpartition[i] != selectedpartition[j] ) {
           pay = -selectedpartition[j] * pay;
@@ -170,7 +151,6 @@ void secret::sayigir(){
       //KONTROL çıkan değerin double olması ihtimaline karşı kontrol ettiriyorum.Double ise integer'a çeviriyorum.
       //Basina (int) koy diye dusunurseniz hata edersiniz cunku , den sonraki kisim bizim icin oldukca onemli
       oylemi(interpolasyon);
-      
       results = ((interpolasyonint * partition[tut-1]) % 251);   //Dağıtacağım piksellerim
       //interpolasyonun sonucu negatif ise asagidaki isleme sokuyoruz.
       if((results < 0)){
@@ -179,52 +159,41 @@ void secret::sayigir(){
       else {
         results = ( ( interpolasyonint * partition[tut-1] ) % 251 );
       }
-      
       string Result;
       stringstream convert;
       convert << i;
       Result = convert.str();
-      
       //1.txt 2.txt 3.txt ...
       filename = Result + ".txt";
       dosya.open(filename.c_str(), ios::out | ios::binary| ios::app);
       dosya << results << endl;
     }
   }
-  
-  
+
+
   void pixel::translate(){
-    
     //totaline
     ifstream openFilet("pikseller.txt");
     string linet;
-    
     while (getline(openFilet, linet)) {
       if (linet.empty()) continue;
-      
       istringstream iss(linet);
-      
       nextt = 1 + nextt;
-      
     }
-    
     rgbarray = new int[nextt];
-    
-    
+
     //translate
-    
     for(pt = 0; pt < yedek; pt++){
       string Resultt;
       stringstream convertt;
       convertt << pt;
       Resultt = convertt.str();
       openfilename = Resultt+".txt";
-      
+
       //Assignment
       it = 0;
       ifstream openFile1t(openfilename.c_str());
       string line1t;
-      
       while (getline(openFile1t, line1t)) {
         if (line1t.empty()) continue;
         istringstream iss(line1t);
@@ -232,17 +201,15 @@ void secret::sayigir(){
         rgbarray[it]  = valuet;
         it++;
       }
-      
+
       //11.txt 22.txt 33.txt ...
       filenamet = Resultt + Resultt +".txt";
       ofstream txtt;
       txtt.open(filenamet.c_str(), ios::out | ios::app);
       it = 0;
       jt = 0;
-      
       for (it = 0; it < nextt; ++it)
       {
-        
         if(jt == 1 || jt == 0){
           txtt<<rgbarray[it] << " ";
           jt++;
@@ -260,8 +227,7 @@ void secret::sayigir(){
     }
     imagetranslate();
   }
-  
-  
+
   void pixel::del(){
     it =0;
     for(it=0;it<yedek;it++){
@@ -269,14 +235,13 @@ void secret::sayigir(){
       stringstream converttde;
       converttde << it;
       Resulttde = converttde.str();
-      
       openfilenamede = Resulttde +".txt";
       remove(openfilenamede.c_str());
       openfilenamede = Resulttde + Resulttde +".txt";
       remove(openfilenamede.c_str());
     }
   }
-  
+
   void pixel::imagetranslate(){
     subline = subline/3;
     pt = 0;
@@ -288,19 +253,14 @@ void secret::sayigir(){
       openfilenamei = "./"+ Resultti +".txt";
       FILE *imagee;
       imagee = fopen(openfilenamei.c_str(), "r");
-      
       unsigned char image11[subline][3];
-      
       for (i=0; fscanf (imagee, "%d %d %d", &r, &g, &b) == 3; ++i) {
         image11[i][0] = r;
         image11[i][1] = g;
         image11[i][2] = b;
       }
-      
       fclose(imagee);
-      
-      
-      bitmap_image image(height,width);   //1.si yukseklik 2.si genislik
+      bitmap_image image(height,width);
       image_drawer draw(image);
       int k = 0;
       for (unsigned int d = 0; d < image.width(); ++d)
@@ -309,13 +269,11 @@ void secret::sayigir(){
         {
           image.set_pixel(d,m,image11[k][0],image11[k][1],image11[k][2]);
           k++;
-          
         }
       }
       openfilenamei = Resultti+".bmp";
       image.save_image(openfilenamei.c_str());
     }
-    
     del();
     cout<<"Finish"<<endl;
   }
